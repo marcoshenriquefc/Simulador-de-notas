bb6c5f662d174e89e436283d7870d41e30ab694b
marcoshenriquefc <83990032+marcoshenriquefc@users.noreply.github.com>
2023-08-18 11:15:19 -0300

    feat - remove situation 3 some serie selected

--- assets/scripts/dataUserController.js
+++ assets/scripts/dataUserController.js
@@ -1,8 +1,6 @@
 function putUserName() {
     const $userName = document.querySelector('.username')
-
     const userData = JSON.parse(CookieDataUser.getCookie(CookieDataUser.cookieName));
-
     const nameUser = userData.find( data => {
         return data.name === 'nome';
     })
@@ -10,4 +8,36 @@ function putUserName() {
     $userName.innerText = !!nameUser.value ? `${nameUser.value[0].toUpperCase()}${nameUser.value.substring(1)}` : ''
 }
 
+function verifySerie() {
+    const serieToSituation3 = [
+        '1ano-medio',
+        '2ano-natureza-medio',
+        '2ano-matematica-medio',
+        '2ano-habtecnica-medio',
+        '3ano-natureza-medio',
+        '3ano-matematica-medio',
+        '3ano-habtecnica-medio',
+    ];
+    const userData = JSON.parse(CookieDataUser.getCookie(CookieDataUser.cookieName));
+
+    const serie = userData.find( currentData => {
+        return currentData.name === 'serie'
+    })
+
+    if(serie) {
+        const acceptSituation = serieToSituation3.includes(serie.value);
+
+        if(!acceptSituation) {
+            const $situation3 = document.querySelector('#situation3');
+            $situation3.remove();
+        }
+    }
+    
+    
+
+
+}
+
+verifySerie();
 putUserName();
+
